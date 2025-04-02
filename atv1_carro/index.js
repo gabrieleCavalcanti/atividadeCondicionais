cd53499d17743ba764c29eb680c0f9d8ff4dadda
gabrieleCavalcanti <gabrielefurlancavalcanti@yahoo.com>
2025-04-02 15:23:48 -0300

    Inclementando a resolução da atividade 1

---
+++ atv1_carro/index.js
@@ -0,0 +1,33 @@
+let tipoCarro;
+let kmPercorrido;
+let diasAlugado;
+let valorTotal;
+
+tipoCarro = prompt("Digite L para carro de Luxo e P para carro Popular: ");
+kmPercorrido = parseFloat(prompt("Insira a quantidade de KM percorrido: "));
+diasAlugado = parseInt(prompt("Insira a quantidade de dias alugados"));
+
+
+if ((tipoCarro !== "L" && tipoCarro !== "P") || isNaN(kmPercorrido) || isNaN(diasAlugado)) {
+    alert("Você inseriu alguma informação incorreta,tente novamente");
+} else {
+
+    if (tipoCarro == "P") {
+
+        if (kmPercorrido <= 100) {
+            valorTotal = (diasAlugado * 90) + (kmPercorrido * 0.20);
+        } else {
+            valorTotal = (diasAlugado * 150) + (kmPercorrido * 0.10);
+        }
+
+    } else {
+
+        if (kmPercorrido <= 200) {
+            valorTotal = (diasAlugado * 90) + (kmPercorrido * 0.30);
+        } else {
+            valorTotal = (diasAlugado * 150) + (kmPercorrido * 0.25);
+        }
+    }
+
+    alert(`O valor a ser pago é de R$" ${valorTotal}`)
+}
